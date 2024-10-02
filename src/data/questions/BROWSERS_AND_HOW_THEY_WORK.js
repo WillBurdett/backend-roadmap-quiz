2f6374d0199eb04886d06ad805da3cb1e07890e6
WillBurdett <williamburdett1995@gmail.com>
2024-10-02 14:52:30 +0100

    basic questions added to BROWSERS_AND_HOW_THEY_WORK

--- src/data/questions/BROWSERS_AND_HOW_THEY_WORK.js
+++ src/data/questions/BROWSERS_AND_HOW_THEY_WORK.js
@@ -1,14 +1,120 @@
 export const BROWSERS_AND_HOW_THEY_WORK = [
     {
         questionNumber: 1,
-        question: "What does ‘HTTP’ stand for?",
+        question: "What is the User Interface (UI) for?",
         options: [
-            "HyperText Transmission Protocol",
-            "HyperText Transfer Protocol",
-            "HyperText Transfer Processor"
+            "Stores data e.g. cookies and local storage",
+            "Handles actions from UI e.g. refreshing the page",
+            "Provides visible browser elements e.g. refresh button",
+            "Shows the actual \"website\"",
+            "Handles any network requests",
+            "Interprets the Javascript",
+            "Uses the user interface methods of the underlying operating system. It is mainly used for drawing basic widgets (windows and combo boxes)"
         ],
         answer:
-            "HyperText Transfer Protocol",
+            "Provides visible browser elements e.g. refresh button",
+        minSelection: 1,
+        maxSelection: 1,
+    },
+    {
+        questionNumber: 2,
+        question: "What is the UI Backend for?",
+        options: [
+            "Stores data e.g. cookies and local storage",
+            "Handles actions from UI e.g. refreshing the page",
+            "Provides visible browser elements e.g. refresh button",
+            "Shows the actual \"website\"",
+            "Handles any network requests",
+            "Interprets the Javascript",
+            "Uses the user interface methods of the underlying operating system. It is mainly used for drawing basic widgets (windows and combo boxes)"
+        ],
+        answer:
+            "Uses the user interface methods of the underlying operating system. It is mainly used for drawing basic widgets (windows and combo boxes)",
+        minSelection: 1,
+        maxSelection: 1,
+    },
+    {
+        questionNumber: 3,
+        question: "What is Data Persistence for?",
+        options: [
+            "Stores data e.g. cookies and local storage",
+            "Handles actions from UI e.g. refreshing the page",
+            "Provides visible browser elements e.g. refresh button",
+            "Shows the actual \"website\"",
+            "Handles any network requests",
+            "Interprets the Javascript",
+            "Uses the user interface methods of the underlying operating system. It is mainly used for drawing basic widgets (windows and combo boxes)"
+        ],
+        answer:
+            "Stores data e.g. cookies and local storage",
+        minSelection: 1,
+        maxSelection: 1,
+    },
+    {
+        questionNumber: 4,
+        question: "What is Networking for?",
+        options: [
+            "Stores data e.g. cookies and local storage",
+            "Handles actions from UI e.g. refreshing the page",
+            "Provides visible browser elements e.g. refresh button",
+            "Shows the actual \"website\"",
+            "Handles any network requests",
+            "Interprets the Javascript",
+            "Uses the user interface methods of the underlying operating system. It is mainly used for drawing basic widgets (windows and combo boxes)"
+        ],
+        answer:
+            "Handles any network requests",
+        minSelection: 1,
+        maxSelection: 1,
+    },
+    {
+        questionNumber: 5,
+        question: "What is the Javascript Interpreter for?",
+        options: [
+            "Stores data e.g. cookies and local storage",
+            "Handles actions from UI e.g. refreshing the page",
+            "Provides visible browser elements e.g. refresh button",
+            "Shows the actual \"website\"",
+            "Handles any network requests",
+            "Interprets the Javascript",
+            "Uses the user interface methods of the underlying operating system. It is mainly used for drawing basic widgets (windows and combo boxes)"
+        ],
+        answer:
+            "Interprets the Javascript",
+        minSelection: 1,
+        maxSelection: 1,
+    },
+    {
+        questionNumber: 6,
+        question: "What is the Rendering Engine for?",
+        options: [
+            "Stores data e.g. cookies and local storage",
+            "Handles actions from UI e.g. refreshing the page",
+            "Provides visible browser elements e.g. refresh button",
+            "Shows the actual \"website\"",
+            "Handles any network requests",
+            "Interprets the Javascript",
+            "Uses the user interface methods of the underlying operating system. It is mainly used for drawing basic widgets (windows and combo boxes)"
+        ],
+        answer:
+            "Shows the actual \"website\"",
+        minSelection: 1,
+        maxSelection: 1,
+    },
+    {
+        questionNumber: 7,
+        question: "What is the Browser Engine for?",
+        options: [
+            "Stores data e.g. cookies and local storage",
+            "Handles actions from UI e.g. refreshing the page",
+            "Provides visible browser elements e.g. refresh button",
+            "Shows the actual \"website\"",
+            "Handles any network requests",
+            "Interprets the Javascript",
+            "Uses the user interface methods of the underlying operating system. It is mainly used for drawing basic widgets (windows and combo boxes)"
+        ],
+        answer:
+            "Handles actions from UI e.g. refreshing the page",
         minSelection: 1,
         maxSelection: 1,
     },
